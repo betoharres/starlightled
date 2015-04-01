f66743a73b23633179f0cf6efd87c87cb21d999b
Prelang Builder <builder@prelang.com>
2015-04-01 20:13:34 +0000

    Generated model Product with migration

---
+++ db/migrate/20150401201334_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.string :model
+      t.string :serial_number
+      t.string :mac_address
+      t.string :product_code
+      t.date :fabrication_date
+      t.integer :tension_operation
+      t.references :node, index: true
+
+      t.timestamps
+    end
+  end
+end
