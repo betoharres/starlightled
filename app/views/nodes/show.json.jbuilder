429849ef2e009ffc6ea9e2af66988bd02e33286d
Prelang Builder <builder@prelang.com>
2015-04-01 20:14:35 +0000

    Generated scaffolding for model 'Node' via Rails

---
+++ app/views/nodes/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @node, :id, :identifier, :latitude, :longitude, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :nodes
+
   resources :lamps
 
   resources :gateways
