363348b6d9c6ac6333be4b4d8aa66060674160c0
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-06 13:27:15 -0300

    Fix has one products to product in gateway model

--- app/models/gateway.rb
+++ app/models/gateway.rb
@@ -1,4 +1,4 @@
 class Gateway < ActiveRecord::Base
-  has_one :products, as: :productable, dependent: :destroy
+  has_one :product, as: :productable, dependent: :destroy
   validates_formatting_of :ip, using: :ip_address_v4
 end
