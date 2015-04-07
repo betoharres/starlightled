6b3127b0bb642a49b96aac69b20b86aaeaac362b
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-07 13:49:09 -0300

    add tests to lamps controller to make sure its saving properly with polymorphic association

--- app/models/lamp.rb
+++ app/models/lamp.rb
@@ -1,5 +1,6 @@
 class Lamp < ActiveRecord::Base
   has_one :product, as: :productable, dependent: :destroy
 
+  validates_presence_of :font_type, :font_subtype
   accepts_nested_attributes_for :product
 end
--- spec/factories/lamps.rb
+++ spec/factories/lamps.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :lamp do
     font_type "MyString"
     font_subtype "MyString"
-    product nil
+    product
   end
 
 end
