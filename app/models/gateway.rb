5574842a2a830eff36834e9b87aa07b0fec1a88f
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-07 14:49:19 -0300

    Add accept nested attributes to products in gateways model and add validate presence of ip and url

--- app/models/gateway.rb
+++ app/models/gateway.rb
@@ -1,4 +1,8 @@
 class Gateway < ActiveRecord::Base
   has_one :product, as: :productable, dependent: :destroy
+
+  validates_presence_of :ip, :url_connection
   validates_formatting_of :ip, using: :ip_address_v4
+
+  accepts_nested_attributes_for :product
 end
