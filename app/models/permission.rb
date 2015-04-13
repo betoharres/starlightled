7e4e48eb24116d16c0bed14b57778c5ecb3158c6
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-13 17:55:41 -0300

    Add default values to ability
    The numbers were based on the linux permissions files. Each role will
    have a permission with a number corresponding the ability the user will
    have giving the chosen resource.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,3 +1,17 @@
 class Permission < ActiveRecord::Base
   belongs_to :role
+
+  # like UNIX permissions
+  READ   = 8
+  CREATE = 4
+  UPDATE = 2
+  DELETE = 1
+  enum ability: {
+                 read: READ                     , create: CREATE                 ,
+                 update: UPDATE                 , delete: DELETE                 ,
+                 read_create: READ + CREATE     , read_update: READ + UPDATE     ,
+                 read_delete: READ + DELETE     , create_update: CREATE + UPDATE ,
+                 create_delete: CREATE + DELETE , update_delete: UPDATE + DELETE
+                }
+
 end
