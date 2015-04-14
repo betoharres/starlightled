9370011828e96aab0e0b5c408aff91e63abbd6dc
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-14 14:09:06 -0300

    Pass view tests to Permission
    View tests to permission are now passing. It tests if the passing
    attributes will show in the show and index action after saving it with a
    role.

--- spec/views/permissions/index.html.erb_spec.rb
+++ spec/views/permissions/index.html.erb_spec.rb
@@ -2,24 +2,30 @@ require 'rails_helper'
 
 RSpec.describe "permissions/index", type: :view do
   before(:each) do
+    @role = FactoryGirl.create(:role)
     assign(:permissions, [
       Permission.create!(
-        :resource => "Resource",
-        :ability => 1,
-        :role => nil
+        :resource => "Lamp",
+        :ability => :reads,
+        :role => @role
       ),
       Permission.create!(
-        :resource => "Resource",
-        :ability => 1,
-        :role => nil
+        :resource => "Gateway",
+        :ability => :admin,
+        :role => @role
       )
     ])
   end
 
   it "renders a list of permissions" do
     render
-    assert_select "tr>td", :text => "Resource".to_s, :count => 2
-    assert_select "tr>td", :text => 1.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    expect(rendered).to match(/Lamp/)
+    expect(rendered).to match(/Gateway/)
+    expect(rendered).to match(/reads/)
+    expect(rendered).to match(/admin/)
+    expect(rendered).to match(/#{@role.name}/)
+    # assert_select "tr>td", :text => "Lamp".to_s, :count => 2
+    # assert_select "tr>td", :text => Role.first.to_s, :count => 2
+    # assert_select "tr>td", :text => Permission::READ.to_s, :count => 2
   end
 end
--- spec/views/permissions/show.html.erb_spec.rb
+++ spec/views/permissions/show.html.erb_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe "permissions/show", type: :view do
   before(:each) do
     @permission = assign(:permission, Permission.create!(
-      :resource => "Resource",
-      :ability => 1,
+      :resource => "Lamp",
+      :ability => :admin,
       :role => nil
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    expect(rendered).to match(/Resource/)
-    expect(rendered).to match(/1/)
+    expect(rendered).to match(/Lamp/)
+    expect(rendered).to match(/admin/)
     expect(rendered).to match(//)
   end
 end
