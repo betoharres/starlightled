e546151aa8bd98e2069324b6ab1751efcb1bc316
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-15 18:22:16 -0300

    Change new name of ability in the index view test

--- spec/views/permissions/index.html.erb_spec.rb
+++ spec/views/permissions/index.html.erb_spec.rb
@@ -6,12 +6,12 @@ RSpec.describe "permissions/index", type: :view do
     assign(:permissions, [
       Permission.create!(
         :resource => "Lamp",
-        :ability => :reads,
+        :ability => :can_read,
         :role => @role
       ),
       Permission.create!(
         :resource => "Gateway",
-        :ability => :admin,
+        :ability => :can_all,
         :role => @role
       )
     ])
@@ -21,8 +21,8 @@ RSpec.describe "permissions/index", type: :view do
     render
     expect(rendered).to match(/Lamp/)
     expect(rendered).to match(/Gateway/)
-    expect(rendered).to match(/reads/)
-    expect(rendered).to match(/admin/)
+    expect(rendered).to match(/can_read/)
+    expect(rendered).to match(/can_all/)
     expect(rendered).to match(/#{@role.name}/)
     # assert_select "tr>td", :text => "Lamp".to_s, :count => 2
     # assert_select "tr>td", :text => Role.first.to_s, :count => 2
