716f47f3ad511e490c4a1b1f1bac4c630024e570
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-04-22 21:29:07 -0300

    Add logic to filter unauthorized users
    * Why was this change necessary?
    The need to filter unauthorized users to access or modify data so the
    Admin can dynamically authorize a user.
    
    * How does it address the problem?
    By adding the function deny_access that redirects the user to the root(
    or :back), and executing it inside a case
    statement, and then it authorizes by the action that was called and
    verying if the user has ability to access it.
    
    * Are there any side effects?
    Custom actions may be a future problem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,23 +4,45 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
-  
-  #-> Prelang (user_login:devise)
-  def require_user_signed_in
-    unless user_signed_in?
 
-      # If the user came from a page, we can send them back.  Otherwise, send
-      # them to the root path.
-      if request.env['HTTP_REFERER']
-        fallback_redirect = :back
-      elsif defined?(root_path)
-        fallback_redirect = root_path
-      else
-        fallback_redirect = "/"
-      end
-
-      redirect_to fallback_redirect, flash: {error: "You must be signed in to view this page."}
+  def deny_access
+    # If the user came from a page, we can send them back.  Otherwise, send
+    # them to the root path.
+    if request.env['HTTP_REFERER']
+      fallback_redirect = :back
+    elsif defined?(root_path)
+      fallback_redirect = root_path
+    else
+      fallback_redirect = "/"
     end
+
+    redirect_to fallback_redirect, flash: {error: "You must be signed in to view this page."}
   end
 
+  def require_user_signed_in
+    if !user_signed_in?
+      deny_access
+    else
+      # TODO move the modification to an initializer with all the models in a array
+      model_name = params[:controller].gsub(/Controller/, '').singularize.capitalize
+      ability_number = Permission.abilities[current_user.role.permissions.where(
+        resource: model_name).last.ability]
+        case params[:action]
+          when 'index'
+            deny_access if ability_number < 8
+          when 'show'
+            deny_access if ability_number < 8
+          when 'new'
+            deny_access unless [12, 13, 14, 15].include?(ability_number)
+          when 'create'
+            deny_access unless [12, 13, 14, 15].include?(ability_number)
+          when 'edit'
+            deny_access unless [10, 11, 14, 15].include?(ability_number)
+          when 'update'
+            deny_access unless [10, 11, 14, 15].include?(ability_number)
+          when 'delete'
+            deny_access unless [9, 11, 13, 15].include?(ability_number)
+        end
+    end
+  end
 end
