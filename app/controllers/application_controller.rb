2b9914ce95b84d42c3540f8ba2482b890dd03975
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-05-19 10:51:14 -0300

    Put current_user.role in a var to avoid db hits

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,33 +17,37 @@ class ApplicationController < ActionController::Base
       fallback_redirect = "/"
     end
 
-    redirect_to fallback_redirect, flash: {error: "You must be signed in to view this page."}
+    redirect_to fallback_redirect, flash: {error: "You must be in signed or have permissions to view this page."}
   end
 
   def require_user_signed_in
     if !user_signed_in?
       deny_access
     else
-      # TODO move the modification to an initializer with all the models in a array
-      model_name = params[:controller].gsub(/Controller/, '').singularize.capitalize
-      ability_number = Permission.abilities[current_user.role.permissions.where(
-        resource: model_name).last.ability]
-        case params[:action]
-          when 'index'
-            deny_access if ability_number < 8
-          when 'show'
-            deny_access if ability_number < 8
-          when 'new'
-            deny_access unless [12, 13, 14, 15].include?(ability_number)
-          when 'create'
-            deny_access unless [12, 13, 14, 15].include?(ability_number)
-          when 'edit'
-            deny_access unless [10, 11, 14, 15].include?(ability_number)
-          when 'update'
-            deny_access unless [10, 11, 14, 15].include?(ability_number)
-          when 'destroy'
-            deny_access unless [9, 11, 13, 15].include?(ability_number)
-        end
+      if role = current_user.role
+        # TODO move the modification to an initializer with all the models in a array
+        model_name = params[:controller].gsub(/Controller/, '').singularize.capitalize
+        ability_number = Permission.abilities[role.permissions.where(
+          resource: model_name).last.ability]
+          case params[:action]
+            when 'index'
+              deny_access if ability_number < 8
+            when 'show'
+              deny_access if ability_number < 8
+            when 'new'
+              deny_access unless [12, 13, 14, 15].include?(ability_number)
+            when 'create'
+              deny_access unless [12, 13, 14, 15].include?(ability_number)
+            when 'edit'
+              deny_access unless [10, 11, 14, 15].include?(ability_number)
+            when 'update'
+              deny_access unless [10, 11, 14, 15].include?(ability_number)
+            when 'destroy'
+              deny_access unless [9, 11, 13, 15].include?(ability_number)
+          end
+      else
+        deny_access
+      end
     end
   end
 end
