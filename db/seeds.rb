cec615a5c3a747520347419433f19c2613db8038
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-06-15 16:06:49 -0300

    Add default roles with in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,34 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Create a default user
-user = User.create!(email: 'admin@admin.com', password: 'password',
+user = User.create!(email: 'admin@starlightled.com.br', password: 'password',
                     password_confirmation: 'password')
+
+# Create a default user
+user2 = User.create!(email: 'gerente@starlightled.com.br', password: 'password',
+                    password_confirmation: 'password')
+# Create a default user
+user3 = User.create!(email: 'operador@starlightled.com.br', password: 'password',
+                    password_confirmation: 'password')
+
 company = Company.create!(name: 'StarlightLED', email: 'starlight@gmail.com', user: user)
 user.company = company
-role = Role.create! name: 'SuperUser', company: user.company, users: [user]
+role = Role.create! name: 'Admin', company: user.company, users: [user]
+role2 = Role.create! name: 'Gerente', company: user.company, users: [user2]
+role3 = Role.create! name: 'Operador', company: user.company, users: [user3]
+
 MANAGEABLE_RESOURCES.each do |resource_name|
   role.permissions << Permission.create!(ability: :can_all, resource: resource_name)
+end
+
+MANAGEABLE_RESOURCES.each do |resource_name|
+  if resource_name != 'User'
+    role2.permissions << Permission.create!(ability: :can_all, resource: resource_name)
+  else
+    role2.permissions << Permission.create!(ability: :can_read, resource: resource_name)
+  end
+end
+
+MANAGEABLE_RESOURCES.each do |resource_name|
+  role3.permissions << Permission.create!(ability: :can_read, resource: resource_name)
 end
