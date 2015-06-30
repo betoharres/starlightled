b3f48d29e8bd22bfb5d2f229d4d97c688386176e
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-06-30 17:01:20 -0300

    Add company_id to products table and other fields
    * Why was this change necessary?
    We need a foreign key to point which company that device belongs_to.
    Also we need to register the product code as integer and product
    revision field when the product changes, or get an upgrade
    
    * How does it address the problem?
    Adding those columns
    
    * Are there any side effects?
    Product can't save without company_id

--- db/migrate/20150401201334_create_products.rb
+++ db/migrate/20150401201334_create_products.rb
@@ -3,12 +3,14 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.string :model
-      t.bigint :serial_number, null: false
+      t.bigint :serial_number, null: false # unico por codigo de produto, indice de tabela
       t.string :mac_address
-      t.string :product_code
+      t.integer :product_code, index: true, unique: true # unico para cada familia de produtos, ex: 8000022, indice de tabela
+      t.integer :product_revision # revisao do codigo de produto
       t.date :fabrication_date
       t.integer :tension_operation
       t.references :node, index: true
+      t.references :company, index: true, null: false
       t.references :productable, polymorphic: true, index: true
 
       t.timestamps null: false
