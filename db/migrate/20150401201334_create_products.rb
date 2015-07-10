3253c2868e5b4d35a2ccf46c3f97e6a2bbbbceb1
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-07-10 17:42:08 -0300

    Update products migration file

--- db/migrate/20150401201334_create_products.rb
+++ db/migrate/20150401201334_create_products.rb
@@ -3,10 +3,10 @@ class CreateProducts < ActiveRecord::Migration
     create_table :products do |t|
       t.string :name
       t.string :model
-      t.bigint :serial_number, null: false # unico por codigo de produto, indice de tabela
+      t.bigint :serial_number, index: true, null: false
       t.string :mac_address
-      t.integer :product_code, index: true, unique: true # unico para cada familia de produtos, ex: 8000022, indice de tabela
-      t.integer :product_revision # revisao do codigo de produto
+      t.integer :product_code
+      t.integer :product_revision
       t.date :fabrication_date
       t.integer :tension_operation
       t.references :node, index: true
@@ -15,6 +15,5 @@ class CreateProducts < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_index :products, :serial_number, unique: true
   end
 end
