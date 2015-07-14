32b4f78671e5c3ead531fbcda772c8d857f77050
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-07-14 16:49:13 -0300

    Add audit associated_with: company in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,6 @@ class Product < ActiveRecord::Base
   validates :tension_operation, :numericality => { :greater_than_or_equal_to => 0 }
   validates_presence_of :tension_operation, :name, :model, :serial_number, :mac_address, :product_code, :fabrication_date, :tension_operation
 
+  # https://github.com/collectiveidea/audited
+  audited allow_mass_assignment: true, associated_with: :company
 end
