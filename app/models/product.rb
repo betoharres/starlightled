2a3d7f1037e32e5d1d813ad03f8f28c674c076f6
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-07-27 10:51:06 -0300

    Add final_product method

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,9 @@ class Product < ActiveRecord::Base
   validates :tension_operation, :numericality => { :greater_than_or_equal_to => 0 }
   validates_presence_of :tension_operation, :name, :model, :serial_number, :mac_address, :product_code, :fabrication_date, :tension_operation
 
+  def final_product
+    productable_type.constantize.find(productable_id)
+  end
   # https://github.com/collectiveidea/audited
   audited allow_mass_assignment: true, associated_with: :company
 end
