f87effea3f6bf51a76fd32ab95216fd2b5a12f82
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-07-30 14:59:42 -0300

    Add helper method to retrieve tag name and polymorphic url

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,18 @@
 module TagsHelper
+
+  def tagging_related_path(resource)
+    "/#{resource.taggable_type.downcase.pluralize}/#{resource.taggable_id}"
+  end
+
+  def tagging_related_name(resource)
+    # "#{resource.taggable_type} ##{resource.taggable_id}"
+    case resource.taggable_type
+      when 'Network'
+        Network.find(resource.taggable_id).name
+      when 'Node'
+        Node.find(resource.taggable_id).identifier
+      when 'Lamp'
+        Lamp.find(resource.taggable_id).product.name
+    end
+  end
 end
