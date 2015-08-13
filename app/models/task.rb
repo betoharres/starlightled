b8b3b7aef5e3b524b3eea708f3d023f87d2789df
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-08-13 13:09:10 -0300

    Add aasm states and before_save method in Task model
    Add before_save with execute_at_to_utc method to make sure the date for
    execute_at is being saved as UTC timezone.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,16 @@
 class Task < ActiveRecord::Base
   include AASM
-
   belongs_to :node
+  before_save :execute_at_to_utc
+
+  aasm do
+    state :waiting, :initial => true
+    state :running
+    state :failed
+    state :done
+  end
+
+  def execute_at_to_utc
+    execute_at = execute_at.utc
+  end
 end
