43eca26613e641a617ab9a5ff7385ecb4bcad9ae
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-08-14 10:15:12 -0300

    Change Tasks table fields
    To improve performance and db design, it limit the progress size to 2
    bytes, remove url field and config_files references.
    Add attachable field as polymorphic to be able to point if the record
    belongs to a config_file or a firmware table.

--- db/migrate/20150814024123_create_tasks.rb
+++ db/migrate/20150814024123_create_tasks.rb
@@ -4,12 +4,11 @@ class CreateTasks < ActiveRecord::Migration
       t.datetime :execute_at
       t.integer :code, limit: 2
       t.integer :priority, limit: 2
-      t.integer :progress, default: 0
+      t.integer :progress, default: 0, litmit: 2
       t.string :aasm_state
       t.string :description
-      t.string :url
       t.references :node, index: true, foreign_key: true
-      t.references :config_file, index: true, foreign_key: true
+      t.references :attachable, polymorphic: true, index: true
       t.references :company, index: true, foreign_key: true
 
       t.timestamps null: false
