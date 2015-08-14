c0dafbcb7ce417c811fc18f1bf30fade92b745c2
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-08-14 17:08:08 -0300

    Add validation and description field to ConfigFile

--- app/models/config_file.rb
+++ app/models/config_file.rb
@@ -1,6 +1,7 @@
 class ConfigFile < ActiveRecord::Base
   belongs_to :company
   has_many :tasks
+  validates :name, presence: true
 
   audited allow_mass_assignment: true, associated_with: :company
 end
