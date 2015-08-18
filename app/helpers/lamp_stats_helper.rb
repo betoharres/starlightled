e1770015df54ed7508c43d1c92231c4b289f60e7
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-08-18 16:08:10 -0300

    Add data types, colors and format seconds
    lamp_stat#show

--- app/helpers/lamp_stats_helper.rb
+++ app/helpers/lamp_stats_helper.rb
@@ -1,2 +1,9 @@
 module LampStatsHelper
+  def formatted_seconds(total_seconds)
+    hours = total_seconds / (60 * 60)
+    minutes = (total_seconds / 60) % 60
+    seconds = total_seconds % 60
+
+    "#{ hours }:#{ minutes }:#{ seconds }"
+  end
 end
