c3c2af7fe5c44b36a25671083e38651977a6c385
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-08-18 10:17:43 -0300

    Filter tasks json fields
    Removes useless fields in the tasks json reponse of lamp_stat#create

--- app/controllers/lamp_stats_controller.rb
+++ app/controllers/lamp_stats_controller.rb
@@ -36,6 +36,7 @@ class LampStatsController < ApplicationController
     product = Product.find_by(serial_number: @lamp_stat.serial_num)
     @tasks = Task.where(aasm_state: :waiting, company_id: product.company_id).order(:execute_at).last
 
+    @tasks = @tasks.to_json(only: [:id, :code, :attachable_id, :attachable_type])
     respond_to do |format|
       if @lamp_stat.save
         format.html { redirect_to @lamp_stat, notice: 'Lamp stat was successfully created.' }
