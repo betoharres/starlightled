a55c44096681ba9ee327f8c9914228ba9780b75f
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-08-26 14:07:09 -0300

    Remove set_config_file in update action
    This prevents updates request coming from gateway of failing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
   before_action :require_user_signed_in, except: [:update]
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :set_config_file, only: [:new, :edit, :update, :create]
+  before_action :set_config_file, only: [:new, :edit, :create]
 
   # GET /tasks
   # GET /tasks.json
