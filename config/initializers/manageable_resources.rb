5aa04b2aacd7693d0a2fba1a28f379b3f819ba0a
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-09-10 11:11:22 -0300

    Fix login and Lamp Tests
    Also add resource option when creating stub for logged user

--- config/initializers/manageable_resources.rb
+++ config/initializers/manageable_resources.rb
@@ -1,4 +1,5 @@
 Rails.application.eager_load!
 resources_array ||= ActiveRecord::Base.descendants.map(&:name)
 blacklist ||= ["Audited::Adapters::ActiveRecord::Audit", "NetworksNode"]
+resources_array << 'Landing'
 MANAGEABLE_RESOURCES ||= resources_array.reject { |h| blacklist.include? h }
--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,8 +1,8 @@
 # stub sign_in method https://github.com/plataformatec/devise/wiki/How-To:-Stub-authentication-in-controller-specs
 module ControllerHelpers
 
-  def auth_user(ability = :can_all)
-    permission = FactoryGirl.create(:permission, ability: ability)
+  def auth_user(ability = :can_all, resource = 'Lamp')
+    permission = FactoryGirl.create(:permission, ability: ability, resource: resource)
     authorized_user = FactoryGirl.create(:user, role: permission.role)
     authorized_user.company = FactoryGirl.create(:company, user: authorized_user)
     authorized_user
