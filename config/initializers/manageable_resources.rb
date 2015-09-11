a9158adc9c849ca8fcdf2ca3c5a8f2264b4aaed0
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-09-11 10:44:03 -0300

    Add Audit to MANAGEABLE_RESOURCES and remove TagType

--- config/initializers/manageable_resources.rb
+++ config/initializers/manageable_resources.rb
@@ -1,5 +1,6 @@
 Rails.application.eager_load!
 resources_array ||= ActiveRecord::Base.descendants.map(&:name)
-blacklist ||= ["Audited::Adapters::ActiveRecord::Audit", "NetworksNode"]
 resources_array << 'Landing'
+resources_array << 'Audit'
+blacklist ||= ["Audited::Adapters::ActiveRecord::Audit", "NetworksNode", "TagType"]
 MANAGEABLE_RESOURCES ||= resources_array.reject { |h| blacklist.include? h }
