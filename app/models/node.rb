ce6fa5287dab0827736cc8ec28d17d24dc56ee63
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-09-21 13:32:59 -0300

    Add validation to name of network and identifier in node

--- app/models/node.rb
+++ app/models/node.rb
@@ -4,7 +4,7 @@ class Node < ActiveRecord::Base
 
   belongs_to :network
   belongs_to :company
-  validates  :network, presence: true
+  validates  :network, :identifier, presence: true
 
   acts_as_taggable
   audited allow_mass_assignment: true, associated_with: :company
