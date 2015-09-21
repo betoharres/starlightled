6f6d70880a665c9381abf5da89fdc8da48629ca1
Alberto Harres Rocha <albertohrocha@gmail.com>
2015-09-21 16:49:36 -0300

    Fix pending tests for roles_controller_spec
    Also add validation to the name of the role in the model

--- app/models/role.rb
+++ app/models/role.rb
@@ -3,5 +3,7 @@ class Role < ActiveRecord::Base
   belongs_to :company
   has_many :permissions
 
+  validates :name, presence: true
+
   audited allow_mass_assignment: true, associated_with: :company
 end
