66da0940133f062ff5caa6e89ccb19b43120c341
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-05-11 11:07:03 -0300

    Change seed to user starlight info

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,16 @@
 
 # SELT
 ###################################################
-user = User.create!(email: 'admin@selt.com.br', password: 'Selt3354',
-                    password_confirmation: 'Selt3354')
-
-user.company = Company.create!(name: 'Selt Engenharia', email: 'selt@selt.com.br', user: user)
-
-role = Role.create! name: 'Admin', company: user.company, users: [user]
-
-MANAGEABLE_RESOURCES.each do |resource_name|
-  role.permissions << Permission.create!(ability: :can_all, resource: resource_name)
-end
+# user = User.create!(email: 'admin@selt.com.br', password: 'Selt3354',
+#                     password_confirmation: 'Selt3354')
+#
+# user.company = Company.create!(name: 'Selt Engenharia', email: 'selt@selt.com.br', user: user)
+#
+# role = Role.create! name: 'Admin', company: user.company, users: [user]
+#
+# MANAGEABLE_RESOURCES.each do |resource_name|
+#   role.permissions << Permission.create!(ability: :can_all, resource: resource_name)
+# end
 
 # DC Park
 ####################################################
@@ -35,37 +35,37 @@ end
 # StarlightLED
 ####################################################
 
-# # Create a default user
-# user = User.create!(email: 'admin@starlightled.com.br', password: 'password',
-#                     password_confirmation: 'password')
-#
-# # Create a default user
-# user2 = User.create!(email: 'gerente@starlightled.com.br', password: 'password',
-#                     password_confirmation: 'password')
-# # Create a default user
-# user3 = User.create!(email: 'operador@starlightled.com.br', password: 'password',
-#                     password_confirmation: 'password')
-#
-# company = Company.create!(name: 'StarlightLED', email: 'starlight@starlightled.com.br', user: user)
-# user.company = company
-# user2.company = company
-# user3.company = company
-# role = Role.create! name: 'Admin', company: user.company, users: [user]
-# role2 = Role.create! name: 'Gerente', company: user.company, users: [user2]
-# role3 = Role.create! name: 'Operador', company: user.company, users: [user3]
-#
-# MANAGEABLE_RESOURCES.each do |resource_name|
-#   role.permissions << Permission.create!(ability: :can_all, resource: resource_name)
-# end
-#
-# MANAGEABLE_RESOURCES.each do |resource_name|
-#   if resource_name != 'User'
-#     role2.permissions << Permission.create!(ability: :can_all, resource: resource_name)
-#   else
-#     role2.permissions << Permission.create!(ability: :can_read, resource: resource_name)
-#   end
-# end
-#
-# MANAGEABLE_RESOURCES.each do |resource_name|
-#   role3.permissions << Permission.create!(ability: :can_read, resource: resource_name)
-# end
+# Create a default user
+user = User.create!(email: 'admin@starlightled.com.br', password: 'password',
+                    password_confirmation: 'password')
+
+# Create a default user
+user2 = User.create!(email: 'gerente@starlightled.com.br', password: 'password',
+                    password_confirmation: 'password')
+# Create a default user
+user3 = User.create!(email: 'operador@starlightled.com.br', password: 'password',
+                    password_confirmation: 'password')
+
+company = Company.create!(name: 'StarlightLED', email: 'starlight@starlightled.com.br', user: user)
+user.company = company
+user2.company = company
+user3.company = company
+role = Role.create! name: 'Admin', company: user.company, users: [user]
+role2 = Role.create! name: 'Gerente', company: user.company, users: [user2]
+role3 = Role.create! name: 'Operador', company: user.company, users: [user3]
+
+MANAGEABLE_RESOURCES.each do |resource_name|
+  role.permissions << Permission.create!(ability: :can_all, resource: resource_name)
+end
+
+MANAGEABLE_RESOURCES.each do |resource_name|
+  if resource_name != 'User'
+    role2.permissions << Permission.create!(ability: :can_all, resource: resource_name)
+  else
+    role2.permissions << Permission.create!(ability: :can_read, resource: resource_name)
+  end
+end
+
+MANAGEABLE_RESOURCES.each do |resource_name|
+  role3.permissions << Permission.create!(ability: :can_read, resource: resource_name)
+end
