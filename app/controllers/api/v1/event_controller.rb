e85597aae77ae2b35ff425ad8a28bbd6081f106a
Alberto Harres Rocha <albertohrocha@gmail.com>
2016-05-17 17:17:46 -0300

    Remove format html in event create action

--- app/controllers/api/v1/event_controller.rb
+++ app/controllers/api/v1/event_controller.rb
@@ -38,10 +38,8 @@ module Api
 
         respond_to do |format|
           if @event.save
-            format.html { redirect_to @event, notice: 'Event was successfully created.' }
             format.json { render :show, status: :created, location: @event }
           else
-            format.html { render :new }
             format.json { render json: @event.errors, status: :unprocessable_entity }
           end
         end
